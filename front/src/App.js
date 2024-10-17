487aafd973342ac8714c80e00d05c18c1305d3e6
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-10-17 18:20:07 -0500

    Creations 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
+import Home from './Components/Pages/Home/Home';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
