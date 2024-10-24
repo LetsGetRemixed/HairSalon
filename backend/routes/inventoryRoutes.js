3aedb5924c683140a9bddfe8333d248da1291ee9
Isaiah Gage <frb32@txstate.edu>
2024-10-24 18:02:48 -0500

    Updated routes and subscription seperate fucntiosn for post and put

--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -4,6 +4,8 @@ const inventoryController = require('../controllers/inventoryController');
 
 router.post('/addItem', inventoryController.addItem);
 router.get('/allItems', inventoryController.getAllInventory);
+router.get('/getItem/:id', inventoryController.getOneItem);
+router.patch('/updateQuantity', inventoryController.updateQuantity);
 
 
 module.exports = router;
--- backend/routes/subscriptionRoutes.js
+++ backend/routes/subscriptionRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 
 // Route to create or extend a membership
-router.post('/:userId/membership', subscriptionController.createMembership);
+router.post('/create-membership/:userId', subscriptionController.createMembership);
+router.put('/update-membership/:userId', subscriptionController.updateMembership);
 
 module.exports = router;
