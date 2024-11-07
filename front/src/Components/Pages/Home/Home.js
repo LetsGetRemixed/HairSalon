54226c7c796e8ce060786490a7d71fc0398193b4
Isaiah Gage <frb32@txstate.edu>
2024-11-07 12:13:26 -0600

    ADDED IMAGES!!!

--- front/src/Components/Pages/Home/Home.js
+++ front/src/Components/Pages/Home/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from '../Universal/Footer';
 import Navbar2 from '../Universal/Navbar2';
 
 
 const Home = () => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    // Fetch image URLs from the backend
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('http://localhost:5100/api/images/all-images'); // Adjust to your backend URL
+        const data = await response.json();
+        setImages(data.images); // Assuming data.images is the array of URLs
+        console.log('images are set');
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
   return (
     <div>
 
@@ -14,6 +30,14 @@ const Home = () => {
                       <Header />
                       <div className="font-cinzel text-3xl bg-mainBackground  py-24">
                          PRODUCTS
+                         {images.map((url, index) => (
+        <img
+          key={index}
+          src={url}
+          alt={`Image ${index + 1}`}
+          style={{ width: '200px', height: '200px', margin: '10px', objectFit: 'cover' }}
+        />
+      ))}
                          <div className="font-cinze flex items-center justify-center text-2xl bg-mainBackground py-24">
                           Hair Extensions
                          </div>
