9e299dd782100c0242eb9840869beb106927e8ec
Isaiah Gage <frb32@txstate.edu>
2024-11-13 10:38:24 -0600

    Fixed issue

--- front/src/Components/Pages/Home/Home.js
+++ front/src/Components/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import Footer from '../Universal/Footer';
 import Navbar2 from '../Universal/Navbar2';
