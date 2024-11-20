cae95eda6ec8c2a778c8194470e5c8bbbad0b9ad
Isaiah Gage <frb32@txstate.edu>
2024-11-19 19:52:01 -0600

    Added get all inventory route

--- front/src/Components/Pages/Home/Home.js
+++ front/src/Components/Pages/Home/Home.js
@@ -4,22 +4,7 @@ import Footer from '../Universal/Footer';
 import Navbar2 from '../Universal/Navbar2';
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    // Fetch products from "Blonde" category
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('http://localhost:5100/api/items/getByCategory/Blonde');
-        const data = await response.json();
-        console.log(data);
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
-    fetchProducts();
-  }, []);
+  
 
   return (
     <div>
@@ -28,22 +13,6 @@ const Home = () => {
         <Header />
         <div className="font-cinzel text-3xl bg-mainBackground py-24">
           <h1>PRODUCTS</h1>
-          <div className="font-cinze flex flex-col items-center justify-center text-2xl bg-mainBackground py-24">
-            <h2>Hair Extensions</h2>
-            <div className="grid grid-cols-3 gap-4 mt-8">
-              {products.map((product) => (
-                <div key={product._id} className="border rounded-lg p-4">
-                  <img
-                    src={product.imageUrl}
-                    
-                    className="w-full h-auto object-cover rounded-md"
-                  />
-                  <h3 className="mt-4 text-lg">{product.name}</h3>
-                  
-                </div>
-              ))}
-            </div>
-          </div>
         </div>
       </div>
       <Footer />
