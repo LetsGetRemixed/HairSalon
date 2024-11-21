854c9c84b67378cf3ec954702aace85f73a8fab6
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-11-20 20:49:32 -0600

    Subscription bar

--- front/src/Components/Pages/Home/Home.js
+++ front/src/Components/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from '../Universal/Footer';
 import Navbar2 from '../Universal/Navbar2';
 import Products from '../Products/ProductsHome';
+import SubscriptionHome from '../Sucbription/SubscriptionHome';
 
 const Home = () => {
 
@@ -11,7 +12,7 @@ const Home = () => {
       <Navbar2 />
                 <div className="font-cinzel text-3xl ">
                       <Header />
-                      
+                      <SubscriptionHome />
                       <Products />
 
                 </div>
