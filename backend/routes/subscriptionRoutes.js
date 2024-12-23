abcfeb41128b917b4b14692014c67ad279302584
Isaiah Gage <frb32@txstate.edu>
2024-12-22 23:21:28 -0600

    maybe

--- backend/routes/subscriptionRoutes.js
+++ backend/routes/subscriptionRoutes.js
@@ -4,7 +4,6 @@ const subscriptionController = require('../controllers/subscriptionController');
 
 // Route to create or extend a membership
 router.post('/create-membership/:userId', subscriptionController.createMembership);
-router.put('/update-membership/:userId', subscriptionController.updateMembership);
 router.get('/check-user-subscription/:userId', subscriptionController.getSubscriptionByUserId);
 router.get('/get-all-subscriptions', subscriptionController.getAllSubscriptions);
 router.patch('/cancel-subscription/:userId', subscriptionController.cancelSubscription);
