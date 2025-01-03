347d344582b717007cbbb20cc97f9e6a0f474c5a
Isaiah Gage <frb32@txstate.edu>
2025-01-03 17:25:31 -0600

    Added cron job to update expired subs back to stylist and sets expire date to null

--- backend/controllers/subscriptionController.js
+++ backend/controllers/subscriptionController.js
@@ -123,40 +123,6 @@ exports.getSubscriptionByUserId = async (req, res) => {
   }
 }
 
-// Cancel a subscription and set a expiration date at the end of a billing period
-exports.cancelSubscription = async (req, res) => {
-  const { userId } = req.params; 
-
-  try {
-    const user = await User.findById(userId).populate('subscription'); 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const subscription = user.subscription;
-    if (!subscription) {
-      return res.status(404).json({ message: 'No active subscription found for user' });
-    }
-    // Cancel the subscription
-    const updatedSubscription = await stripe.subscriptions.update(subscription.subscriptionId, {
-      cancel_at_period_end: true,
-    });
-    
-    const currentPeriodEnd = new Date(updatedSubscription.current_period_end * 1000);
-    subscription.expireDate = currentPeriodEnd;
-    await subscription.save();
-
-    res.status(200).json({
-      message: 'Subscription will cancel at the end of the current billing period',
-      updatedSubscription,
-      subscription,
-    });
-  } catch (error) {
-    console.error('Error canceling subscription:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
 // Update the membership type for a user
 exports.updateSubscriptionStatus = async (req, res) => {
   const { userId } = req.params;
@@ -189,29 +155,63 @@ exports.updateSubscriptionStatus = async (req, res) => {
   }
 };
 
-// Update all of the expired subscriptions to Styilst 
-exports.checkAndUpdateExpiredSubscription = async (req, res) => {
+// Cancel a subscription and set a expiration date at the end of a billing period
+exports.cancelSubscription = async (req, res) => {
+  const { userId } = req.params; 
+
+  try {
+    const user = await User.findById(userId).populate('subscription'); 
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const subscription = user.subscription;
+    if (!subscription) {
+      return res.status(404).json({ message: 'No active subscription found for user' });
+    }
+    // Cancel the subscription
+    const updatedSubscription = await stripe.subscriptions.update(subscription.subscriptionId, {
+      cancel_at_period_end: true,
+    });
+    
+    const currentPeriodEnd = new Date(updatedSubscription.current_period_end * 1000);
+    subscription.expireDate = currentPeriodEnd;
+    subscription.nextBillDate = null;
+    await subscription.save();
+
+    res.status(200).json({
+      message: 'Subscription will cancel at the end of the current billing period',
+      updatedSubscription,
+      subscription,
+    });
+  } catch (error) {
+    console.error('Error canceling subscription:', error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+cron.schedule('0 0 * * *', async () => {
   try {
     const now = new Date();
+    // Find all of the expired subscriptions
     const expiredSubscriptions = await Subscription.find({ 
       expireDate: { $lt: now }, 
       isActive: true 
     });
-    console.log(expiredSubscriptions);
-
-    // Check all active subscriptions
+    // Update all of the expired subscriptions
     for (const subscription of expiredSubscriptions) {
       subscription.membershipType = 'Stylist';
       subscription.isActive = true;
+      subscription.expireDate = null;
       await subscription.save();
       console.log(`Updated subscription ${subscription._id} to inactive.`);
     }
-    res.json({ message: 'Subscriptions that are expired are now set to false' });
   } catch (error) {
     console.error('Error checking subscriptions subscription:', error);
-    res.status(500).json({ error: error.message });
   }
-};
+})
+
 
 exports.upgradeMembership = async (req, res) => {
   const { userId } = req.params;
